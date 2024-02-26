ff6c72a6dd010826c5e870d33677c30cb8c2f8d7
hdelcarpiom <140546274+hdelcarpiom@users.noreply.github.com>
2024-02-26 12:28:15 -0500

    unificacion de ejerciicos

---
+++ 01_RegresionLineal/regresion_lineal_celsius_fahrenheit.py
@@ -0,0 +1,5 @@
+#Primer ejercicio de progresion lineal
+import pandas as pd
+datos = pd.read_csv('01RegresionLinealCelsiusFahrenheit/datos.csv')
+datos.info() # informacion del conjunto de datos
+datos.head()

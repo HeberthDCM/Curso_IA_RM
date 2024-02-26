5287664f0df9a1a774812fbb2fa4f3fc27f80fb9
hdelcarpiom <140546274+hdelcarpiom@users.noreply.github.com>
2024-02-26 17:12:13 -0500

    cambio1

--- 01_RegresionLineal/regresion_lineal_celsius_fahrenheit.py
+++ 01_RegresionLineal/regresion_lineal_celsius_fahrenheit.py
@@ -1,5 +1,11 @@
 #Primer ejercicio de progresion lineal
 import pandas as pd
-datos = pd.read_csv('01_RegresionLineal/datos.csv')
-datos.info() # informacion del conjunto de datos
-datos.head()
+import seaborn as sb
+
+t_temperatura = pd.read_csv('01_RegresionLineal/datos.csv')
+#t_temperatura.info() # informacion del conjunto de datos
+t_temperatura.head()
+
+
+sb.scatterplot(data=t_temperatura, x='celsius', y='fahrenheit')
+input('Presione una tecka')
